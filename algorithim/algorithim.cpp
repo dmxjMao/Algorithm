99a0ddf3c6dda961f2235426b863e7805f786514
dmxjMao <dmxjMao0@zyhyjqr>
2017-08-30 23:43:53 +0800

    demo 包含字符指针的结构体如何读写文件。

--- algorithim/algorithim.cpp
+++ algorithim/algorithim.cpp
@@ -5,109 +5,99 @@
 #include <iostream>
 #include "simple.h"
 #include "bowling.h"//保龄球
+#include <array>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <functional>
+#include <sstream>
+#include <tuple>
+#include <random>
+#include <fstream>
+
+#include <boost\variant.hpp>
+//#include <afxwin.h>
+//#include <atlconv.h>
+#include <Windows.h>
 
 using namespace std;
 
-enum FmtType {
-	INTEGER,
-	FLOAT,
-	LOWERCHAR,
-	UPPERCHAR,
-	ANYCHAR
-};
+struct CFG {
+	//int nSize;
+	TCHAR* szName = nullptr;
+	int ccName = 0;
+	int nValue = 0;
 
-struct CSplitBtn {
-	int a = -1;
+	CFG() {}
+	CFG(/*int sz,*/TCHAR* pName,int nLen,int nValue)
+		:szName(pName), ccName(nLen),nValue(nValue){}
 };
+ostream& operator<<(ostream& wos, const CFG& cfg);
+istream& operator>>(istream& wis, CFG& cfg);
 
-bool MismatchFunc(FmtType t, tuple<FmtType, CSplitBtn*>& p)
-{
-	return t == get<0>(p);
-}
 
 int main()
 {
 	cout << "hello,world!" << endl;
 
-	vector<tuple<FmtType, CSplitBtn*>> vExist
-		= { 
-			{ INTEGER,new CSplitBtn },
-			{ FLOAT,new CSplitBtn },
-			{ ANYCHAR,new CSplitBtn }
-		};
+	vector<CFG> vec = { 
+		CFG(_T("a"), 1, 1),
+		CFG(_T("ab"), 2, 2),
+		CFG(_T("爱彼此"), 3, 3)
+	};
 
-	//增删改
-	vector<FmtType> v = { INTEGER,ANYCHAR };
+	ofstream ofs("爱彼此", ios::binary);
+	//TCHAR* p = _T("你好,世界！");
+	char* p = "mao";
+	ofs << p;
+	ofs.close();
 
-	size_t vLen = v.size(), vExistLen = vExist.size();
+	ifstream ifs("爱批次", ios::binary);
+	//int l = strlen(p);
+	//char* p2 = new char[6];
+	char p2[6] = { 0 };
+	//p2[5] = 0;
+	//ifs.read(p2,5);
+	string line;
+	getline(ifs, line);
 
-	/*元素不匹配的位置迭代器，有end(), E 代表已存在的元素，v代表动态变化的元素*/
-	using misPos_vE = pair<decltype(v)::iterator, decltype(vExist)::iterator>;
-	//using rmisPos_vE = pair<decltype(v)::reverse_iterator, decltype(vExist)::reverse_iterator>;
-	using misPos_Ev = pair<decltype(vExist)::iterator, decltype(v)::iterator>;
-	//using rmisPos_Ev = pair<decltype(vExist)::reverse_iterator, decltype(v)::reverse_iterator>;
+	ifs.close();
 
-	using namespace std::placeholders;
-	
-	//mismatch元素少的放前面
-	if (vLen > vExistLen) {//增加, 动态元素多
-		auto f = bind(MismatchFunc, _2, _1);
-		//查不匹配位置，在vExist中构造增加的元素
-		misPos_Ev misPos = mismatch(vExist.begin(), vExist.end(), v.begin(), f);
-		vExist.erase(misPos.first, vExist.end());
-		for (auto it = misPos.second; it != v.end(); ++it) {
-			//构造空的按钮
-			vExist.emplace_back(make_tuple(*it, nullptr));
-		}
-		
-		//哪个位置开始不匹配
-		//size_t misPos = distance(v.begin(), pEv.second);
-		//反向查找一次不匹配
-		//rmisPos_Ev prEv = mismatch(vExist.rbegin(), vExist.rend(), v.rbegin(), f);
-		////反向哪个位置开始不匹配
-		//size_t rmisPos = distance(prEv.second, v.rend()) - 1;/*vLen - distance(v.rbegin(), prEv.second) - 1;*/
-		//有一种v=112, vExist=12的情况：反向位置<正向  1234 和 12334都是
-		//if (rmisPos < misPos) {
-		//	rmisPos = misPos +
-		//		distance(vExist.begin(), pEv.first)
-		//		- distance(prEv.first, vExist.rend()) - 1 ;
-		//}
-		/* rmisPos - misPos 是要插入元素的个数 */
-		//auto tmpIt = pEv.first;//正向不匹配位置
-		//while (misPos <= rmisPos) {//遍历所有要插入的元素
-		//	tmpIt = vExist.insert(tmpIt, make_tuple(*(v.begin() + rmisPos++),nullptr));
-		//	++tmpIt; //vector在前面插入,这样就在后面插入了
-		//}
-	}
-	else if(vLen < vExistLen){//删,动态元素少
-		misPos_vE misPos = mismatch(v.begin(), v.end(), vExist.begin(), MismatchFunc);
-		vExist.erase(misPos.second, vExist.end());
-		for (auto it = misPos.first; it != v.end(); ++it) {
-			vExist.emplace_back(make_tuple(*it, nullptr));
-		}
-		//misPos_vE pvE = mismatch(v.begin(), v.end(), vExist.begin(), MismatchFunc);//正向查找一次不匹配
-		//size_t pvEpos = distance(vExist.begin(), pvE.second);//第几个元素开始为删除的元素，从多的取
-		//rmisPos_vE prvE = mismatch(v.rbegin(), v.rend(), vExist.rbegin(), MismatchFunc);//反向查找一次不匹配
-		//size_t prvEpos = vExistLen - distance(vExist.rbegin(), prvE.second) - 1;
+	//ofstream ofs("struct_ptr2file", ios::binary);
+	//if (ofs.fail())
+	//	return -1;
+	//const auto& cfg = vec[0];
+	//ofs << cfg;
+	//ofs.close();
 
-		//auto eIt1 = pvE.second;
-		//auto eIt2 = eIt1 + prvEpos - pvEpos;
-		//vExist.erase(eIt1, eIt2);
-
-	}
-	else {//改
-		//misPair_vm p = mismatch(v.begin(), v.end(), m.begin(), MismatchFunc);
-		//FmtType tv = *p.first;
-		//FmtType tm = p.second->first;
-		//cout << "FmtType add:" << tv << "\tdelete:" << tm << endl;
-	}
-	
+	//ifstream ifs("struct_ptr2file", ios::binary);
+	//if (ifs.fail())
+	//	return -1;
+	//CFG cfg2;
+	//ifs >> cfg2;
+	//ifs.close();
 
 
     return 0;
 }
 
+
+ostream& operator<<(ostream& os, const CFG& cfg)
+{
+	os << cfg.ccName << cfg.nValue;
+	int charlen = 2 * cfg.ccName;
+	char* p = new char[charlen + 1];
+	wcstombs_s(nullptr, p, charlen, cfg.szName, charlen);
+	os << p;
+	return os;
+}
+
+istream& operator >> (istream& is, CFG& cfg)
+{
+	is >> cfg.ccName >> cfg.nValue;
+	//TCHAR* p = new TCHAR[cfg.cbName]; //包含\0
+	//char* p = new char[2 * cfg.ccName + 1];
+	//is >> p;
+	//mbstowcs(cfg.szName,p,)
+	return is;
+}
